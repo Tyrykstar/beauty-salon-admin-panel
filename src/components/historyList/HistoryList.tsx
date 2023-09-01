fb61aa549652510e725509b2809398ec70bd464c
Artur Petryshyn <tyrykstar@gmail.com>
2023-09-01 17:31:51 +0300

    History list and 404 page

--- src/components/historyList/HistoryList.tsx
+++ src/components/historyList/HistoryList.tsx
@@ -1,14 +1,34 @@
 import AppointmentItem from "../appointmentItem.tsx/AppointmentItem";
+import { useEffect, useContext, useCallback, useState } from "react";
+import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
+import App from "../app/App";
 
 function HistoryList() {
+	const [isOpen, setIsOpen] = useState(false);
+	const [selectedId, setSelectedId] = useState(0);
+
+	const { allAppointments, getAppointments } = useContext(AppointmentContext);
+
+	const handleOpenModal = useCallback((id: number) => {
+		setIsOpen(true);
+		setSelectedId(id);
+	}, []);
+
+	useEffect(() => {
+		getAppointments();
+	}, []);
+
+	console.log("allAppointments = > ", allAppointments);
+
 	return (
 		<>
-			{/* <AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem /> */}
+			{allAppointments.map((item) => (
+				<AppointmentItem
+					key={item.id}
+					{...item}
+					openModal={handleOpenModal}
+				/>
+			))}
 		</>
 	);
 }
---
+++ src/pages/404/404.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Error from "../../components/error/Error";
+import { useNavigate } from "react-router-dom";
+import "./404.scss";
+
+const PageNotFound = () => {
+	const navigate = useNavigate();
+	return (
+		<div className="page-not-found">
+			<Error />
+			<h1>This page wasn't found</h1>
+			<button onClick={() => navigate(-1)}>Go to revious page</button>
+		</div>
+	);
+};
+
+export default PageNotFound;
